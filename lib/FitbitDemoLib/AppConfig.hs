5d1ae224203a7ebf84cfb3cb16bb25e2de3a6c07
Richard Cook <rcook@rcook.org>
2018-02-19 14:12:21 -0800

    Documentation

--- lib/FitbitDemoLib/AppConfig.hs
+++ lib/FitbitDemoLib/AppConfig.hs
@@ -7,11 +7,14 @@ module FitbitDemoLib.AppConfig
 import           Data.Aeson ((.:), (.=), FromJSON(..), ToJSON(..), object, withObject)
 import           FitbitDemoLib.FitbitAPI
 
+-- | Application configuration
 data AppConfig = AppConfig FitbitAPI deriving Show
 
+-- | 'FromJSON' instance for deserializing application configuration from YAML
 instance FromJSON AppConfig where
     parseJSON = withObject "AppConfig" $ \v -> AppConfig <$> v .: "fitbit-api"
 
+-- | 'ToJSON' instance for serializing application configuration to YAML
 instance ToJSON AppConfig where
     toJSON (AppConfig fitbitAPI) =
         object
--- lib/FitbitDemoLib/FitbitAPI.hs
+++ lib/FitbitDemoLib/FitbitAPI.hs
@@ -7,17 +7,20 @@ module FitbitDemoLib.FitbitAPI
 import           Data.Aeson ((.:), (.=), FromJSON(..), ToJSON(..), object, withObject)
 import           FitbitDemoLib.Types
 
+-- | Fitbit API configuration
 data FitbitAPI = FitbitAPI ClientId ClientSecret deriving Show
 
+-- | 'FromJSON' instance for deserializing Fitbit API configuration from YAML
 instance FromJSON FitbitAPI where
     parseJSON =
         withObject "FitbitAPI" $ \v -> FitbitAPI
             <$> (ClientId <$> v .: "client-id")
             <*> (ClientSecret <$> v .: "client-secret")
 
+-- | 'ToJSON' instance for serializing Fitbit API configuration to YAML
 instance ToJSON FitbitAPI where
-    toJSON (FitbitAPI (ClientId clientId) (ClientSecret clientSecret)) =
+    toJSON (FitbitAPI (ClientId cid) (ClientSecret cs)) =
         object
-            [ "client-id" .= clientId
-            , "client-secret" .= clientSecret
+            [ "client-id" .= cid
+            , "client-secret" .= cs
             ]
