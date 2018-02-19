1819ecf5680d95d1dd9d220e4b60da82b0e8b261
Richard Cook <rcook@rcook.org>
2018-02-19 10:18:01 -0800

    Write token config

--- app/Main.hs
+++ app/Main.hs
@@ -26,6 +26,8 @@ import           FitbitDemoApp
 import           GHC.Generics
 import           Network.HTTP.Req
 import           Network.HTTP.Req.Url.Extra (toUrlHttps)
+import           System.Directory (doesFileExist, getHomeDirectory)
+import           System.FilePath ((</>))
 import           Text.URI.QQ (uri)
 
 data AccessTokenRequest = AccessTokenRequest ClientId AuthCode deriving Show
@@ -52,26 +54,52 @@ instance FromJSON AccessTokenResponse where
 doIt :: Url 'Https -> AuthCode -> ClientId -> ClientSecret -> IO (Either String AccessTokenResponse)
 doIt url authCode@(AuthCode ac) clientId@(ClientId cid) clientSecret = runReq def $ do
     let opts = header "Authorization" (ByteString.append "Basic " (encodeClientAuth clientId clientSecret))
-        --tokenReq = AccessTokenRequest clientId authCode
-        bbb = "code" =: ac <> "grant_type" =: ("authorization_code" :: Text) <> "client_id" =: cid
-    body <- responseBody <$> req POST url (ReqBodyUrlEnc bbb) jsonResponse opts
+        formBody = "code" =: ac <> "grant_type" =: ("authorization_code" :: Text) <> "client_id" =: cid <> "expires_in" =: ("3600" :: Text)
+    body <- responseBody <$> req POST url (ReqBodyUrlEnc formBody) jsonResponse opts
     return $ parseEither parseJSON body
 
 encodeClientAuth :: ClientId -> ClientSecret -> ByteString
 encodeClientAuth (ClientId cId) (ClientSecret s) = Base64.encode $ ByteString.concat [Text.encodeUtf8 cId, ":", Text.encodeUtf8 s]
 
-
-main :: IO ()
-main = do
-    {-
+-- TODO: Move to Config
+readTokenConfig :: IO TokenConfig
+readTokenConfig = do
     Just (Config (FitbitAPI clientId clientSecret)) <- getConfig
     authCode <- getAuthCode clientId
     let Just (url, _) = toUrlHttps [uri|https://api.fitbit.com/oauth2/token|]
     result <- doIt url authCode clientId clientSecret
-    print result
-    putStrLn "Done"
-    -}
-    Just (TokenConfig (AccessToken ac) refreshToken) <- getTokenConfig
+    let (AccessTokenResponse at rt) = case result of
+                                        Left e -> error e
+                                        Right x -> x
+        tokenConfig = TokenConfig at rt
+    tokenConfigPath <- getTokenConfigPath
+    encodeYAMLFile tokenConfigPath tokenConfig
+    return tokenConfig
+
+-- TODO: Move to Config
+configDir :: FilePath
+configDir = ".fitbit-demo"
+
+-- TODO: Move to Config
+getTokenConfigPath :: IO FilePath
+getTokenConfigPath = do
+    homeDir <- getHomeDirectory
+    return $ homeDir </> configDir </> "token.yaml"
+
+-- TODO: Move to Config
+myGetTokenConfig :: IO TokenConfig
+myGetTokenConfig = do
+    tokenConfigPath <- getTokenConfigPath
+    tokenConfigPathExists <- doesFileExist tokenConfigPath
+    if tokenConfigPathExists
+        then do
+            Just tokenConfig <- getTokenConfig
+            return tokenConfig
+        else readTokenConfig
+
+main :: IO ()
+main = do
+    TokenConfig (AccessToken ac) refreshToken <- myGetTokenConfig
     let ac' = Text.encodeUtf8 ac
     result <- responseBody <$> (runReq def $
                 req GET
