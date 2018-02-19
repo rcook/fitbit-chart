bba83be8b336b8d5af3768ac8c4848c3208c8703
Richard Cook <rcook@rcook.org>
2018-02-19 14:14:25 -0800

    Documentation

--- lib/FitbitDemoLib/Types.hs
+++ lib/FitbitDemoLib/Types.hs
@@ -7,10 +7,14 @@ module FitbitDemoLib.Types
 
 import           Data.Text (Text)
 
+-- | Fitbit API client ID
 newtype ClientId = ClientId Text deriving Show
 
+-- | Fitbit API client secret
 newtype ClientSecret = ClientSecret Text deriving Show
 
+-- | Fitbit API access token
 newtype AccessToken = AccessToken Text deriving Show
 
+-- | Fitbit API refresh token
 newtype RefreshToken = RefreshToken Text deriving Show
