2d68739c81a64ac18fe260dffe7a4a490d4c1459
Richard Cook <rcook@rcook.org>
2018-03-31 13:38:32 -0700

    Remove unused init.cmd

--- script/init.cmd
+++
@@ -1,2 +0,0 @@
-@echo off
-aws --endpoint-url=http://localhost:4569 dynamodb create-table --table-name weight-samples --attribute-definitions AttributeName=date,AttributeType=S --key-schema AttributeName=date,KeyType=HASH --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5
