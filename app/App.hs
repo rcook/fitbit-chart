6f9f3e2cd78721dde773cab4db8780f65aa48aad
Richard Cook <rcook@rcook.org>
2018-04-09 20:31:01 -0700

    Rename Config to AWSConfig

--- app/App.hs
+++ app/App.hs
@@ -1,15 +1,15 @@
 module App
-    ( module App.AppConfig
+    ( module App.AWSConfig
+    , module App.AppConfig
     , module App.CommandLine
-    , module App.Config
     , module App.Log
     , module App.TokenConfig
     , module App.Util
     ) where
 
+import App.AWSConfig
 import App.AppConfig
 import App.CommandLine
-import App.Config
 import App.Log
 import App.TokenConfig
 import App.Util
--- app/App/AWSConfig.hs
+++ app/App/AWSConfig.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module App.Config
+module App.AWSConfig
     ( getAWSConfigFromEnv
     , getEnvRegion
     , getAWSConfig
