4a0e0c3132d38fb6cd3fd0eb3a6b298b84457bfd
Richard Cook <rcook@rcook.org>
2018-04-17 08:40:19 -0700

    Update to Lambda

---
+++ script/lib/lambda.rb
@@ -0,0 +1,28 @@
+require_relative 'shell'
+
+module Lambda
+  def self.create_function(function_name, zip_path, runtime, role, handler)
+    zip_path = "fileb://#{zip_path}"
+    status, output = Shell.capture(
+      'aws',
+      'lambda',
+      'update-function-code',
+      '--function-name', function_name,
+      '--zip-file', zip_path)
+    return if status.success?
+
+    unless output.include?('ResourceNotFoundException')
+      raise "update-function-code command failed: output=[#{output}] status=[#{status}]"
+    end
+
+    Shell.check_capture(
+      'aws',
+      'lambda',
+      'create-function',
+      '--function-name', function_name,
+      '--zip-file', zip_path,
+      '--runtime', runtime,
+      '--role', role,
+      '--handler', handler)
+  end
+end
---
+++ script/upload.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+# -*-ruby-*-
+# vi:syntax=ruby
+require_relative 'lib/lambda'
+require_relative 'lib/manifest'
+
+def make_package(manifest, repo_dir)
+  lambda_package = manifest.lambda_package
+  function_name = lambda_package.function_name
+  runtime = lambda_package.runtime
+  zip_path = File.expand_path('.package-work/output/fitbit-chart-lambda.zip', repo_dir)
+  role = lambda_package.role
+  handler = lambda_package.handler
+  Lambda.create_function function_name, zip_path, runtime, role, handler
+end
+
+def main
+  this_dir = File.expand_path('..', __FILE__)
+  repo_dir = File.dirname(this_dir)
+  manifest = Manifest.new(File.expand_path('manifest.yaml', repo_dir))
+  make_package manifest, repo_dir
+end
+main
