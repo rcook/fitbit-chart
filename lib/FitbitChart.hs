467d4de9dd453003f8acfda4f54485357f0af649
Richard Cook <rcook@rcook.org>
2018-04-20 07:37:08 -0700

    Fix umbrella module

--- lib/FitbitChart.hs
+++ lib/FitbitChart.hs
@@ -1,13 +1,19 @@
 module FitbitChart
     ( module FitbitChart.AWS
     , module FitbitChart.DataAccess
+    , module FitbitChart.DynamoDB
     , module FitbitChart.Errors
     , module FitbitChart.FitbitAPI
+    , module FitbitChart.S3
+    , module FitbitChart.SSM
     , module FitbitChart.Util
     ) where
 
 import FitbitChart.AWS
 import FitbitChart.DataAccess
+import FitbitChart.DynamoDB
 import FitbitChart.Errors
 import FitbitChart.FitbitAPI
+import FitbitChart.S3
+import FitbitChart.SSM
 import FitbitChart.Util
