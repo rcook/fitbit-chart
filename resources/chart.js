36a36c401cd6c91da665578b97d31ada8b1ed31c
Richard Cook <rcook@rcook.org>
2018-04-21 21:03:41 -0700

    Report latest sample

--- resources/chart.js
+++ resources/chart.js
@@ -5,9 +5,11 @@ $(function() {
       var date = new Date(parseInt(parts[0]), parseInt(parts[1]) - 1, parseInt(parts[2]));
       return [[date, item[1]]];
     });
+
     d.sort(function (a, b) {
       return a[0] - b[0];
     });
+
     $.plot("#chart", [d], {
       xaxis: {
         mode: "time",
@@ -17,6 +19,13 @@ $(function() {
         min: 180
       }
     });
+
+    var count = d.length;
+    var lastDate = d[count - 1][0];
+    var p = $("<p>");
+    p.text(`Most recent sample: ${lastDate.toString()}`);
+    p.insertAfter("#chart");
+
   }).fail(function (e) {
     console.dir(e);
     alert("ERROR: " + e.status);
